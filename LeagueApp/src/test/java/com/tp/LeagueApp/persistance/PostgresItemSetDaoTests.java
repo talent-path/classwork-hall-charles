d4086be25aea261277f64ef88641f4167fb1d51f
Charles Hall <charleshall@C02DFWLKML7H.attlocal.net>
2021-02-10 15:55:48 -0500

    Partial test for createItemSet golden path

--- LeagueApp/src/test/java/com/tp/LeagueApp/persistance/PostgresItemSetDaoTests.java
+++ LeagueApp/src/test/java/com/tp/LeagueApp/persistance/PostgresItemSetDaoTests.java
@@ -1,11 +1,17 @@
 package com.tp.LeagueApp.persistance;
 
+import com.tp.LeagueApp.models.ItemSet;
 import com.tp.LeagueApp.persistance.postgres.PostgresItemSetDao;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("daoTesting")
@@ -27,6 +33,24 @@ public class PostgresItemSetDaoTests {
 
     }
 
-    
+    @Test
+    public void createNewItemSetGoldenPath() {
+        ItemSet itemSetToAdd = new ItemSet();
+        itemSetToAdd.setItemSetName("Testing Item Set");
+        itemSetToAdd.setChampionId(1);
+
+        ItemSet returnedItemSet = toTest.createNewItemSet(itemSetToAdd);
+
+        assertEquals( 1, returnedItemSet.getItemSetId() );
+        assertEquals( "Testing Item Set", returnedItemSet.getItemSetName() );
+        assertEquals( 1, returnedItemSet.getChampionId());
+
+        List<ItemSet> allItemSets = toTest.getAllItemSets();
+
+        assertEquals( 1, allItemSets.get(0).getItemSetId() );
+        assertEquals( "Testing Item Set", allItemSets.get(0).getItemSetName() );
+        assertEquals( 1, allItemSets.get(0).getChampionId());
+
+    }
 
 }
