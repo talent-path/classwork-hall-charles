e850ee7d05a24e1c6b3afe378f4e84ee68afa72d
Charles Hall <charleshall@C02DFWLKML7H.attlocal.net>
2021-02-10 17:33:49 -0500

    Added golden path tests for remaining create set methods.

--- LeagueApp/src/test/java/com/tp/LeagueApp/persistance/PostgresRuneSetDaoTests.java
+++ LeagueApp/src/test/java/com/tp/LeagueApp/persistance/PostgresRuneSetDaoTests.java
@@ -1,11 +1,18 @@
 package com.tp.LeagueApp.persistance;
 
+import com.tp.LeagueApp.models.ItemSet;
+import com.tp.LeagueApp.models.RuneSet;
 import com.tp.LeagueApp.persistance.postgres.PostgresRuneSetDao;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("daoTesting")
@@ -27,5 +34,25 @@ public class PostgresRuneSetDaoTests {
 
         template.update("insert into \"Champions\" (\"championName\", \"championDescription\",\"winRate\",\"pickRate\",\"banRate\",\"avgKDA\")\n" +
                 "values ('Test','Test Description', '1','1','1','1')");
+    }
+
+    @Test
+    public void createNewItemSetGoldenPath() {
+        RuneSet runeSetToAdd = new RuneSet();
+        runeSetToAdd.setRuneSetName("Testing Rune Set");
+        runeSetToAdd.setChampionId(1);
+
+        RuneSet returnedRuneSet = toTest.createNewRuneSet(runeSetToAdd);
+
+        assertEquals( 1, returnedRuneSet.getRuneSetId() );
+        assertEquals( "Testing Rune Set", returnedRuneSet.getRuneSetName() );
+        assertEquals( 1, returnedRuneSet.getChampionId());
+
+        List<RuneSet> allRuneSets = toTest.getAllRuneSets();
+
+        assertEquals( 1, allRuneSets.get(0).getRuneSetId() );
+        assertEquals( "Testing Rune Set", allRuneSets.get(0).getRuneSetName() );
+        assertEquals( 1, allRuneSets.get(0).getChampionId());
+
     }
 }
--- LeagueApp/src/test/java/com/tp/LeagueApp/persistance/PostgresSummonerSpellSetDaoTests.java
+++ LeagueApp/src/test/java/com/tp/LeagueApp/persistance/PostgresSummonerSpellSetDaoTests.java
@@ -1,11 +1,17 @@
 package com.tp.LeagueApp.persistance;
 
+import com.tp.LeagueApp.models.SummonerSpellSet;
 import com.tp.LeagueApp.persistance.postgres.PostgresSummonerSpellSetDao;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("daoTesting")
@@ -27,5 +33,24 @@ public class PostgresSummonerSpellSetDaoTests {
 
         template.update("insert into \"Champions\" (\"championName\", \"championDescription\",\"winRate\",\"pickRate\",\"banRate\",\"avgKDA\")\n" +
                 "values ('Test','Test Description', '1','1','1','1')");
+    }
+
+    @Test
+    public void createNewSummonerSpellSetGoldenPath() {
+        SummonerSpellSet summSetToAdd = new SummonerSpellSet();
+        summSetToAdd.setSummonerSpellSetName("Testing Summ Set");
+        summSetToAdd.setChampionId(1);
+
+        SummonerSpellSet returnedSummonerSpellSet = toTest.createNewSummonerSpellSet(summSetToAdd);
+
+        assertEquals( 1, returnedSummonerSpellSet.getSummonerSpellSetId() );
+        assertEquals( "Testing Summ Set", returnedSummonerSpellSet.getSummonerSpellSetName() );
+        assertEquals( 1, returnedSummonerSpellSet.getChampionId());
+
+        List<SummonerSpellSet> allItemSets = toTest.getAllSummonerSpellSets();
+
+        assertEquals( 1, allItemSets.get(0).getSummonerSpellSetId() );
+        assertEquals( "Testing Summ Set", allItemSets.get(0).getSummonerSpellSetName() );
+        assertEquals( 1, allItemSets.get(0).getChampionId());
     }
 }
