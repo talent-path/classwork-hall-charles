d98e4c19f83b9b540d6152c7d2133e80c3a305af
Charles Hall <charleshall@C02DFWLKML7H.attlocal.net>
2021-05-25 16:59:24 -0400

    Problem3 made.

---
+++ ProjectEuler/Problem3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Numerics;
+using Utils;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //What is the largest prime factor of the number 600851475143 ?
+
+            BigInteger input = 600851475143;
+
+            BigInteger squareRoot = Util.GetSquareRoot(input);
+
+            BigInteger biggestLowFactor = int.MinValue;
+
+            bool found = false;
+
+            for(BigInteger i = 1; i <= squareRoot; i++)
+            {
+                if(input % i == 0)
+                {
+                    BigInteger bigFactor = input / i;
+
+                    if(Util.IsPrime(bigFactor))
+                    {
+                        Console.WriteLine(bigFactor);
+                        found = true;
+                        break;
+                    }
+
+                    if(Util.IsPrime(i))
+                    {
+                        biggestLowFactor = i;
+                    }
+                }
+            }
+
+            if(!found)
+            {
+                Console.WriteLine(biggestLowFactor);
+            }
+        }
+    }
+}
---
+++ ProjectEuler/Utils/Util.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Numerics;
+
+namespace Utils
+{
+    public static class Util
+    {
+        public static bool IsPrime(BigInteger num)
+        {
+            bool prime = true;
+
+            //Number is prime if..
+            //1 2 3 4 6 12
+            //1 12
+            //2 6
+            //3 4
+
+            BigInteger squareRoot = GetSquareRoot(num);
+
+            //Handle 2 special case
+            if(num % 2 == 0)
+            {
+                return num == 2;
+            }
+
+            for(BigInteger i = 3; i <= squareRoot; i+=2)
+            {
+                if(num % i == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+
+            return prime;
+        }
+
+        public static BigInteger GetSquareRoot(BigInteger num)
+        {
+            BigInteger squareRoot = 0;
+
+            for(BigInteger i = 1; i < num; i++)
+            {
+               if((i*i) > num)
+               {
+                    squareRoot = i - 1;
+                    break;
+               }
+            }
+
+            return squareRoot;
+        }
+    }
+}
