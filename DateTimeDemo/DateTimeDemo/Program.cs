3dbbc14880ecabdc85d8c54c8245ad42469976bd
Charles Hall <charleshall@C02DFWLKML7H.attlocal.net>
2021-05-25 14:28:46 -0400

    Date Time Problem

---
+++ DateTimeDemo/DateTimeDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DateTimeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a date (yyyy-MM-dd):");
+            string userInput = Console.ReadLine();
+
+            DateTime userDate = DateTime.ParseExact(userInput, "yyyy-MM-dd", null);
+
+            int daysTilFriday = System.DayOfWeek.Friday - userDate.DayOfWeek;
+
+            DateTime dateOfFriday = userDate.AddDays(daysTilFriday);
+
+            Console.WriteLine("User Date: " + userDate);
+            Console.WriteLine("Date of next Friday: " + dateOfFriday);
+            Console.WriteLine("Days until next Friday: " + daysTilFriday);
+
+        }
+    }
+}
