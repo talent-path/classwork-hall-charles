f833c46fcfb08279b3681bdcae47997e079be28a
Charles Hall <charleshall@C02DFWLKML7H.attlocal.net>
2021-05-26 09:40:07 -0400

    Problem 9

--- ProjectEuler/Problem9/Program.cs
+++ ProjectEuler/Problem9/Program.cs
@@ -7,7 +7,21 @@ namespace Problem9
         static void Main(string[] args)
         {
             //There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
+            int product = 0;
 
+            for(int a = 1; a < 1000; a++)
+            {
+                for(int b = 1; b < 1000; b++)
+                {
+                    int c = 1000 - (a + b);
+                    if((a * a) + (b * b) == (c * c))
+                    {
+                        product = a * b * c;
+                    }
+                }
+            }
+
+            Console.WriteLine(product);
         }
     }
 }
