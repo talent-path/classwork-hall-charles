9ed0e29568405b909a77b61ad1aca722be5715bb
Charles Hall <charleshall@C02DFWLKML7H.attlocal.net>
2021-05-26 09:18:35 -0400

    Optimized problem 7, added problems

--- ProjectEuler/Problem7/Program.cs
+++ ProjectEuler/Problem7/Program.cs
@@ -7,13 +7,13 @@ namespace Problem7
     {
         static void Main(string[] args)
         {
-            int count = 0;
-            for(int i = 0; i < int.MaxValue && count <= 10001; i++)
+            int count = 1;
+            for(int i = 3; i < int.MaxValue && count <= 10001; i+=2)
             {
                 if(Util.IsPrime(i))
                 {
                     count++;
-                    if (count == 10002)
+                    if (count == 10001)
                     {
                         Console.WriteLine(i);
                     }
---
+++ ProjectEuler/Problem9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Problem9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
+
+        }
+    }
+}
