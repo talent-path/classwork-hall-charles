a93f0bc7776177efb140fbe14b4eb0dde2bd6956
Charles Hall <charleshall@C02DFWLKML7H.attlocal.net>
2021-06-03 14:00:49 -0400

    Web Crawler MVP

---
+++ WebCrawler/WebCrawler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter the URL you want to get.");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(userInput);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                await File.WriteAllTextAsync("../../../html.txt", responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+            
+        }
+    }
+}
