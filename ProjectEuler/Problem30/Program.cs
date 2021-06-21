1a8549d84a50b3326ea663a97af60734a92ba575
Charles Hall <charleshall@C02DFWLKML7H.attlocal.net>
2021-06-21 12:45:08 -0400

    Warmup

---
+++ ProjectEuler/Problem30/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Problem30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int curSum = 0;
+
+            for(int cur = 10; cur < 999999; cur++)
+            {
+                curSum = 0;
+                int copy = cur;
+                while(copy != 0)
+                {
+                    int d = copy % 10;
+                    int fifth = d * d * d * d * d;
+                    curSum += fifth;
+                    copy /= 10;
+                }
+
+                if (curSum == cur)
+                {
+                    sum += curSum;
+                }
+
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
