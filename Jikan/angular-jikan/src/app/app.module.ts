8d46da6ce43e99034c78f9a041c6ed2b0bd0bc02
Charles Hall <chall@talentpath.com>
2021-06-30 13:42:00 -0400

    Added starter components

--- Jikan/angular-jikan/src/app/app.module.ts
+++ Jikan/angular-jikan/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ItemDisplayComponent } from './components/item-display/item-display.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { CheckoutPageComponent } from './components/checkout-page/checkout-page.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    ItemDisplayComponent,
+    ItemDetailComponent,
+    SearchPageComponent,
+    ContactPageComponent,
+    CheckoutPageComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
