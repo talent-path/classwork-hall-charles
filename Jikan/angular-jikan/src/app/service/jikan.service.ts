6e5c41293f49680a86fbd0de671d1632a61a3a2d
Charles Hall <chall@talentpath.com>
2021-06-30 14:44:23 -0400

    Organization

--- Jikan/angular-jikan/src/app/service/jikan.service.ts
+++ Jikan/angular-jikan/src/app/service/jikan.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import {tap, catchError} from 'rxjs/operators';
-import { Watch } from './models/Watch';
-import { Order } from './models/Order';
+import { Watch } from '../models/Watch';
+import { Order } from '../models/Order';
 
 @Injectable({
   providedIn: 'root'
