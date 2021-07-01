e16e53d6805905cd797a05b48da16df63f0ea102
Charles Hall <chall@talentpath.com>
2021-07-01 16:42:53 -0400

    Nicer watch display for individual watches and watch list. Updated navbar and home page

--- Jikan/angular-jikan/src/app/app-routing.module.ts
+++ Jikan/angular-jikan/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { OrdersListComponent } from './components/orders-list/orders-list.component';
 import { WatchesListComponent } from './components/watches-list/watches-list.component';
 
 const routes: Routes = [
   { path: 'watches', component: WatchesListComponent },
-  { path: 'orders', component: OrdersListComponent}
+  { path: 'orders', component: OrdersListComponent },
+  {path: 'home', component: HomePageComponent }
 ];
 
 @NgModule({
