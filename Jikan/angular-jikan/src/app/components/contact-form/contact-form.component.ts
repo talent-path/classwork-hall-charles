8b4423905bf0edad5c809747fe1c63aaccb93df5
Charles Hall <chall@talentpath.com>
2021-07-06 13:58:10 -0400

    Contact Form works

--- Jikan/angular-jikan/src/app/components/contact-form/contact-form.component.ts
+++ Jikan/angular-jikan/src/app/components/contact-form/contact-form.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+  
+  ngOnInit() {
+  }
 
-  ngOnInit(): void {
+  onSubmit(contactForm: NgForm) {
+    if (contactForm.valid) {
+      const email = contactForm.value;
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      this.http.post('https://formspree.io/f/mdoydvej',
+        { name: email.name, replyto: email.email, message: email.messages },
+        { 'headers': headers }).subscribe(
+          response => {
+            console.log(response);
+          }
+        );
+    }
+    var verifySent = <HTMLDivElement>document.getElementById("messageSent");
+    verifySent.innerHTML = "Thank You! Your Message Has Been Sent.";
+    this.clearForm();
+  }
+
+  clearForm()
+  {
+    (<HTMLFormElement>document.getElementById("contactForm")).reset();
   }
 
 }
