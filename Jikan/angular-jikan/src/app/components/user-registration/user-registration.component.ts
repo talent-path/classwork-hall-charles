3ee90781d825af90ca25daf4b05249334d25a30c
Charles Hall <chall@talentpath.com>
2021-07-14 14:47:45 -0400

    User registration styling and takes to home page after registration.

--- Jikan/angular-jikan/src/app/components/user-registration/user-registration.component.ts
+++ Jikan/angular-jikan/src/app/components/user-registration/user-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { RegisterUserRequest } from 'src/app/models/RegisteredUserRequest';
 import { AuthService } from 'src/app/service/auth.service';
 
@@ -15,21 +16,27 @@ export class UserRegistrationComponent implements OnInit {
   password : string = "";
   confirmPass : string = "";
 
-  constructor(private authService : AuthService) { }
+  constructor(private authService : AuthService, private router : Router) { }
 
   ngOnInit(): void {
   }
 
   submit() {
 
-    let toRegister : RegisterUserRequest = {
-      Username : this.username,
-      Name : this.name,
-      Email : this.email,
-      Password : this.password,
-    }
+    if(this.password == this.confirmPass)
+    {
+      let toRegister : RegisterUserRequest = {
+        Username : this.username,
+        Name : this.name,
+        Email : this.email,
+        Password : this.password,
+      }
+      console.log(toRegister)
 
-    this.authService.registerUser(toRegister).subscribe((_) => console.log(_));
+      this.authService.registerUser(toRegister).subscribe((_) => console.log(_));
+      this.router.navigate([""]);
+
+    }
   }
 
 }
